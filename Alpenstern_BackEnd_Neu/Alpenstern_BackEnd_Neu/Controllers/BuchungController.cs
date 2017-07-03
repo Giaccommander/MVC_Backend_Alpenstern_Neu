5f9cf8b7000a8eaf3f967ed128f7805f12bbd300
m.babic_admin <m.babic_admin@PC23>
2017-07-03 16:06:17 +0200

    Controller hinzugefügt

---
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/BuchungController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpenstern_BackEnd_Neu.Controllers
+{
+    public class BuchungController : Controller
+    {
+        // GET: Buchung
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

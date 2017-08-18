a735f4c28e868fdddfa262800e55e378f8645d40
insanius_mpsp3 <insanius@users.noreply.github.com>
2017-08-18 10:25:56 +0200

    reg und login

--- Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/HomeController.cs
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/HomeController.cs
@@ -3,23 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Alpenstern_BackEnd_Neu.Models;
 
 namespace Alpenstern_BackEnd_Neu.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+
+            //Gast gast = new Gast();
+
+            //gast.nachname = "Mustermann";
+            //gast.vorname = "Max";
+
+            //using (var datenbank = new alpensternEntities())
+            //{
+            //    var gast = datenbank.Gast.Find(1);
+
+            //    //var zinr = gast.Anfrage.ToList()[0].Kategorieanfrage.ToList()[0]
+            //    //    .Zimmerbuchung.ToList()[0].Zimmer.zimmerNummer;
+
+            //    var gastVM = new GastVM();
+
+            //    gastVM.Nachname = gast.nachname;
+            //    gastVM.Vorname = gast.vorname;
+
+            //    return View(gastVM);
+            //}
+
+            return View();
+                
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Index(LoginVM lvm)
+        {
+
+            //1. Existiert der Login? Wenn nicht, "return", wenn ja: 2
+            //2. Mitarbeiter aus der DB holen
+            //3. Salt an das eingegebene PW anhaengen
+            //4. PW+Salt hashen 
+            //5. Hashes vergleichen, Wenn gleich: 6, wenn ungleich: "return"
+            
+            //6. Authorisierung vornehmen
+
+
+            using (var datenbank = new alpensternEntities())
+            {
+                var loginStrings = lvm.Login.Split('.');
+                var vmVorname = loginStrings[0];
+                var vmNachname = loginStrings[1];
+
+                //1
+                if (datenbank.Mitarbeiter.Any(n => n.nachname == vmNachname && n.vorname == vmVorname))
+                {   //2
+                    var dbMitarbeiter = datenbank.Mitarbeiter.Where((n => n.nachname == vmNachname && n.vorname == vmVorname)).FirstOrDefault();
+                    //3
+                    lvm.Passwort += dbMitarbeiter.salt;
+                    //4
+                    var eingegPWHash = Helper.Hashes.HashBerechnen(lvm.Passwort);
+                    //5
+                    if(eingegPWHash == dbMitarbeiter.passwort)
+                    {
+                        //6
+                        var authTicket = new FormsAuthenticationTicket(
+                            1,  //Ticketversion
+                            lvm.Login, //Useridentifizierung
+                            DateTime.Now, //Zeitpunkt der Erstellung
+                            DateTime.Now.AddMinutes(30), //Wann das Ticket ablaeuft
+                            false, //Persistentes Ticket?
+                            "" //Userdata
+                            );
+
+                        var encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+
+                        var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+
+                        System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
+
+                        return RedirectToAction("Ankunft");
+                    }
+                }
+            }
             return View();
         }
 
+        [Authorize]
         public ActionResult Ankunft()
         {
             ViewBag.Message = "Ankunft Übersicht";
 
             return View();
         }
-
+        [Authorize]
         public ActionResult Rueckruf()
         {
             ViewBag.Message = "Rückruf Service";
---
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Helper/Hashes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Alpenstern_BackEnd_Neu.Helper
+{
+    public static class Hashes
+    {
+
+        private static string HexString(byte[] bytes)
+        {
+            var hashSB = new StringBuilder(64);
+
+            foreach(var b in bytes)
+            {
+                hashSB.Append(b.ToString("X2"));
+            }
+
+            return hashSB.ToString();
+        }
+
+
+        public static string SaltErzeugen()
+        {
+            var salt = new byte[32];
+
+            //Random Number Generator
+            var rng = new RNGCryptoServiceProvider();
+
+            rng.GetNonZeroBytes(salt);
+
+            return HexString(salt);
+        }
+
+        public static string HashBerechnen(string s)
+        {
+            //Daten (in unserem Fall ein String) in ein ByteArray umwandeln
+            var bytes = Encoding.UTF8.GetBytes(s);
+
+            using (SHA256 sha = new SHA256Managed())
+            {
+                var hash = sha.ComputeHash(bytes);
+                return HexString(hash);
+            }
+        }
+    }
+}

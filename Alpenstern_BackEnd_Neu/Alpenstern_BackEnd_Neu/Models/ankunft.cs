0f110faf81c48d38ae2c2dc26a7711bd65dc0d98
patricia.grewe_admin <patricia.grewe_admin@PC31>
2017-08-25 12:04:18 +0200

    beginn

---
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Models/ankunft.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Alpenstern_BackEnd_Neu.Models;
+namespace Alpenstern_BackEnd_Neu.Models
+{
+    public class ankunft
+    {
+
+    }
+}

fcd76074a366bef77f60d30021ed5860bd6a4dca
insanius_mpsp3 <insanius@users.noreply.github.com>
2017-09-01 08:12:33 +0200

    land liste fertigstellen

--- Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/OrtController.cs
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/OrtController.cs
@@ -41,5 +41,39 @@ namespace Alpenstern_BackEnd_Neu.Controllers
 
             return View(landListe);
         }
+
+        [HttpGet]
+        public ActionResult LandDetails(int id)
+        {
+            //View Model erzeugen
+            var vmLandDetails = new LandDetailsVM();
+
+            using(var db = new alpensternEntities())
+            {
+                //Entität aus DB laden
+                var dbLand = db.Land.Find(id);
+
+                //Properties der DB-Entität in das VM Mappen
+                vmLandDetails.Bezeichnung = dbLand.bezeichnung;
+                vmLandDetails.LandId = dbLand.id;
+
+                //Da wir die Städteliste aus der DB (da sie eine DB-Entität ist)
+                //nicht in unser ViewModel abspeichern können...
+                var dbStadtListe = dbLand.Stadt.ToList();
+
+                foreach(var stadt in dbStadtListe)
+                {   //...erzeugen wir für jede DB Stadt
+                    // eine VM Stadt und fügen diese
+                    // unserem View Model (Land Details) hinzu.
+                    var vmStadt = new StadtVM();
+                    vmStadt.Bezeichnung = stadt.bezeichnung;
+                    vmStadt.PLZ = stadt.plz;
+                    vmStadt.StadtId = stadt.id;
+
+                    vmLandDetails.StadtListe.Add(vmStadt);
+                }
+            }
+            return View(vmLandDetails);
+        }
     }
 }

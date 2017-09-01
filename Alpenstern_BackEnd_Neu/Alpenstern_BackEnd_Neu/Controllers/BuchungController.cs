9a71da7ffde9637627bbb06b24e78a3e76014dca
insanius_mpsp3 <insanius@users.noreply.github.com>
2017-09-01 10:00:19 +0200

    Buchungsvorgang Testweise

--- Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/BuchungController.cs
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/BuchungController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Alpenstern_BackEnd_Neu.Models;
 
 namespace Alpenstern_BackEnd_Neu.Controllers
 {
@@ -16,6 +17,58 @@ namespace Alpenstern_BackEnd_Neu.Controllers
         public ActionResult Persoenlichdaten()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult DatumEingabe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DatumEingabe(BuchungVM buchung)
+        {
+            //buchung.gastEmail = User.Identity.Name; //Eigentlich so
+
+            //Das funktioniert nur im Frontend
+            //using (var db = new alpensternEntities())
+            //{
+            //    var dbGast = db.Gast.Where(g => g.email == buchung.gastEmail).FirstOrDefault();
+            //    buchung.gastId = dbGast.id;
+            //}
+
+            buchung.gastEmail = "test@test.com"; //Zum Testen
+            buchung.gastId = 0; //Zum Testen
+
+            //TempData["Buchung"] = buchung;
+
+            Session["Buchung"] = buchung;
+
+            Session["DatumBuchung"] = DateTime.Now;
+
+            Session["randomZahl"] = 42;
+
+            return RedirectToAction("KategorieWahl");
+        }
+
+        [HttpGet]
+        public ActionResult KategorieWahl()
+        {
+            // 1. Ausgewaehltes Datum mittels Prozedur pruefen
+            // 2. Verfuegbare Kategorien laden
+            // 3. Neue Seite anzeigen
+
+            //BuchungVM buchung = (BuchungVM) TempData["Buchung"];
+
+            BuchungVM buchung = (BuchungVM) Session["Buchung"];
+
+            //Verfuegbare Kategorien aus DB holen
+            buchung.verfuegbareKategorien = new Dictionary<int, int>();
+            buchung.verfuegbareKategorien[1] = 5; //normalerweise kommt der Wert aus der DB!
+            buchung.verfuegbareKategorien[2] = 3; //normalerweise kommt der Wert aus der DB!
+            buchung.verfuegbareKategorien[4] = 1; //normalerweise kommt der Wert aus der DB!
+
+            return View(buchung);
         }
     }
 }
---
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Models/BuchungVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alpenstern_BackEnd_Neu.Models
+{
+    public class BuchungVM
+    {
+        //Daten zum abspeichern
+
+        public int gastId { get; set; }
+        public string gastEmail { get; set; }
+
+        public DateTime datumVon { get; set; }
+        public DateTime datumBis { get; set; }
+
+        public Dictionary<int, int> angefragteKategorien { get; set; }
+
+        //Daten zum Anzeigen
+
+        public Dictionary<int, int> verfuegbareKategorien { get; set; }
+    }
+}

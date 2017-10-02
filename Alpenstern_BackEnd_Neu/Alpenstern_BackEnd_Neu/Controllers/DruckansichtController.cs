79232bad07a63057bd2857cc8098383bab0dd8f7
m.babic_admin <m.babic_admin@PC3>
2017-10-02 17:08:15 +0200

    rechnung hinzugefügt

--- Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/DruckansichtController.cs
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Controllers/DruckansichtController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Alpenstern_BackEnd_Neu.Models;
 
 namespace Alpenstern_BackEnd_Neu.Controllers
 {
     public class DruckansichtController : Controller
     {
         // GET: Druckansicht
-        public ActionResult Index()
+        public ActionResult Index(PersonDatenVM vm)
         {
+            using(var db = new alpensternEntities())
+            {
+                var rVM = new RechnungVM();
+
+                rVM.id = vm.id;
+                rVM.vorname = vm.vorname;
+                rVM.nachname = vm.nachname;
+                rVM.strasse = vm.strasse;
+                rVM.plz = vm.plz;
+                rVM.stadt = vm.stadt;
+                rVM.katBezeichnung = vm.katBezeichnung;
+                rVM.kategorieID = vm.kategorieID;
+                rVM.personenAnzahl = vm.personenAnzahl;
+
+
+            }
             return View();
         }
     }
---
+++ Alpenstern_BackEnd_Neu/Alpenstern_BackEnd_Neu/Models/RechnungVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alpenstern_BackEnd_Neu.Models
+{
+    public class RechnungVM
+    {
+        public int id { get; set; }
+        public string vorname { get; set; }
+        public string nachname { get; set; }
+        public string strasse { get; set; }
+        public string stadt { get; set; }
+        public string plz { get; set; }
+        public int zimmerID { get; set; }
+        public int kategorieID { get; set; }
+        public string katBezeichnung { get; set; }
+        public string bezeichnung { get; set; }
+        public decimal preis { get; set; }
+        public int personenAnzahl { get; set; }
+    }
+}
